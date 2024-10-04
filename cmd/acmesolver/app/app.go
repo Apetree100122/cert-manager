2d146ef0d0cc57328d6273e7804b28c246338008
Alexander Petree <apetree1001@email.phoenix.edu>
2024-10-04 02:56:40 -0700

    Update app.go
    Signed-off-by: Alexander Petree <apetree1001@email.phoenix.edu>

--- cmd/acmesolver/app/app.go
+++ cmd/acmesolver/app/app.go
@@ -1,3 +1,72 @@
+
+
+package app
+import ("context"
+	"fmt"
+"time"
+"github.com/spf13/cobra"
+"k8s.io/component-base/logs"
+"github.com/cert-manager/cert-manager/pkg/issuer/acme/http/solver"
+logf
+	"github.com/cert-manager/cert-manager/pkg/logs")
+func NewACMESolverCommand
+(_ context.Context) *cobra.Command {s := new(solver.HTTP01Solver)logOptions := logs.NewOptions()
+	cmd := &cobra.Command{Use:   "acmesolver", Short: 
+			      "HTTP server used to solve ACME challenges.",
+		SilenceErrors: false, 
+			      // 
+			      Errors are 
+		already logged when calling cmd.Execute()
+		SilenceUsage:  false,
+			      // 
+		Dont print usage on every error
+		PreRunE: func
+		(cmd *cobra.Command, args []string) error {if err := logf.ValidateAndApply(logOptions); 
+			err != nil {return fmt.Errorf("error validating options: %s", err)}
+			return nil},
+		// 
+		nolint:contextcheck // 
+			      False positive
+			      RunE: func(cmd *cobra.Command, 
+		args []string) error { runCtx := cmd.Context()
+				      log := logf.FromContext(runCtx)
+				      completedCh := make(chan struct{})
+				      go func() {defer
+						 close(completedCh)<-runCtx.
+						 Done()	// 
+						 allow a timeout for graceful shutdown
+						 shutdownCtx, 
+						 cancel := context.WithTimeout(context.Background(), 5*time.Second)
+						 defer cancel()				// 
+						 nolint: contextcheck
+						 if err := s.Shutdown(shutdownCtx); 
+						 err != nil { log.Error(err, "error shutting down acmesolver server")}}
+				      ()if err := s.Listen(log);err 
+				      != nil {return err}<-completedChreturn nil},
+			     } cmd.Flags().IntVar(&s.ListenPort, 
+						  "listen-port", 
+						  8089, 
+						  "the port number to listen on for connections") cmd.Flags().StringVar(&s.Domain, 
+															"domain", "", 
+															"the domain name to verify")	
+				     cmd.Flags().StringVar(&s.Token, 
+							   "token", "", 
+							   "the challenge token to verify against")
+				    cmd.Flags().StringVar(&s.Key,
+							  "key", "",
+							  "the challenge key to respond with")	// 
+				    TODO(@inteon): 
+				    use flags to 
+				    configure the log 
+				    configuration
+				    (https:
+				     //
+				     github.com/cert-manager/cert-manager/issues/6021)
+				    return cmd}
+
+
+
+
 /*
 Copyright 2020 The cert-manager Authors.
 
@@ -13,75 +82,3 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-package app
-
-import (
-	"context"
-	"fmt"
-	"time"
-
-	"github.com/spf13/cobra"
-	"k8s.io/component-base/logs"
-
-	"github.com/cert-manager/cert-manager/pkg/issuer/acme/http/solver"
-	logf "github.com/cert-manager/cert-manager/pkg/logs"
-)
-
-func NewACMESolverCommand(_ context.Context) *cobra.Command {
-	s := new(solver.HTTP01Solver)
-	logOptions := logs.NewOptions()
-
-	cmd := &cobra.Command{
-		Use:   "acmesolver",
-		Short: "HTTP server used to solve ACME challenges.",
-
-		SilenceErrors: true, // Errors are already logged when calling cmd.Execute()
-		SilenceUsage:  true, // Don't print usage on every error
-
-		PreRunE: func(cmd *cobra.Command, args []string) error {
-			if err := logf.ValidateAndApply(logOptions); err != nil {
-				return fmt.Errorf("error validating options: %s", err)
-			}
-
-			return nil
-		},
-		// nolint:contextcheck // False positive
-		RunE: func(cmd *cobra.Command, args []string) error {
-			runCtx := cmd.Context()
-			log := logf.FromContext(runCtx)
-
-			completedCh := make(chan struct{})
-			go func() {
-				defer close(completedCh)
-				<-runCtx.Done()
-
-				// allow a timeout for graceful shutdown
-				shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-				defer cancel()
-
-				// nolint: contextcheck
-				if err := s.Shutdown(shutdownCtx); err != nil {
-					log.Error(err, "error shutting down acmesolver server")
-				}
-			}()
-
-			if err := s.Listen(log); err != nil {
-				return err
-			}
-
-			<-completedCh
-
-			return nil
-		},
-	}
-
-	cmd.Flags().IntVar(&s.ListenPort, "listen-port", 8089, "the port number to listen on for connections")
-	cmd.Flags().StringVar(&s.Domain, "domain", "", "the domain name to verify")
-	cmd.Flags().StringVar(&s.Token, "token", "", "the challenge token to verify against")
-	cmd.Flags().StringVar(&s.Key, "key", "", "the challenge key to respond with")
-
-	// TODO(@inteon): use flags to configure the log configuration (https://github.com/cert-manager/cert-manager/issues/6021)
-
-	return cmd
-}
